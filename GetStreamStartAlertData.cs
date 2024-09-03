07fbefe44f6fc89342834946b821c6b4aad2cf2f
NuboHeimer <NuboHeimer@yandex.ru>
2024-09-03 22:07:01 +0300

    Поправил форматирование.

--- GetStreamStartAlertData.cs
+++ GetStreamStartAlertData.cs
@@ -15,12 +15,12 @@ public class CPHInline
     {
         string pathToAlertText = args["pathToAlertText"].ToString();
         string alertData = File.ReadAllText(pathToAlertText);
-        return JsonConvert.DeserializeObject<AlertData>(alertData);;
+        return JsonConvert.DeserializeObject<AlertData>(alertData);
     }
+
     public bool GetAllStreamStartAlertData()
     {
         var data = ParseAllStreamStartAlertData();
-
         CPH.SetArgument("annonceText", data.Annonce);
         CPH.SetArgument("game", data.Game);
         CPH.SetArgument("translationTitle", data.TranslationTitle);
@@ -32,46 +32,55 @@ public class CPHInline
         CPH.SetArgument("youTubeFollowers", data.YouTube.Goals.Followers);
         return true;
     }
+
     public bool GetAnnonceText()
     {
         CPH.SetArgument("annonceText", ParseAllStreamStartAlertData().Annonce);
         return true;
     }
+
     public bool GetGame()
     {
         CPH.SetArgument("game", ParseAllStreamStartAlertData().Game);
         return true;
     }
+
     public bool GetTranslationTitle()
     {
         CPH.SetArgument("translationTitle", ParseAllStreamStartAlertData().TranslationTitle);
         return true;
     }
+
     public bool GetVkPlayLiveLink()
     {
         CPH.SetArgument("vkplLink", ParseAllStreamStartAlertData().VKPlayLive.Link);
         return true;
     }
+
     public bool GetVkPlayLiveOnlineGoal()
     {
         CPH.SetArgument("vkplOnilne", ParseAllStreamStartAlertData().VKPlayLive.Goals.Online);
         return true;
     }
-        public bool GetTwitchLink()
+
+    public bool GetTwitchLink()
     {
         CPH.SetArgument("twitchLink", ParseAllStreamStartAlertData().Twitch.Link);
         return true;
     }
+
     public bool GetTwitchFollowersGoal()
     {
         CPH.SetArgument("twitchFollowers", ParseAllStreamStartAlertData().Twitch.Goals.Followers);
         return true;
     }
-            public bool GetYouTubeLink()
+
+    public bool GetYouTubeLink()
     {
         CPH.SetArgument("youTubeLink", ParseAllStreamStartAlertData().YouTube.Link);
         return true;
     }
+
     public bool GetYouTubeFollowersGoal()
     {
         CPH.SetArgument("youTubeFollowers", ParseAllStreamStartAlertData().YouTube.Goals.Followers);
